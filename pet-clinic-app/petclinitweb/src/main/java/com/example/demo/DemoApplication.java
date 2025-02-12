eddeeca327013033ce68af527a143c58ba470ec7
martinj-jaime <martinjosejaime@gmail.com>
2025-02-12 10:45:48 -0300

    agrego logs de version para spring y spring boot

--- pet-clinic-app/petclinitweb/src/main/java/com/example/demo/DemoApplication.java
+++ pet-clinic-app/petclinitweb/src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,15 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootVersion;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.SpringVersion;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Spring Version: " + SpringVersion.getVersion() + " SpringBoot Version:" + SpringBootVersion.getVersion());
 		SpringApplication.run(DemoApplication.class, args);
 	}
 

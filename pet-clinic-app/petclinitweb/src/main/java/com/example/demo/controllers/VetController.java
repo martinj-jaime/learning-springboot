caed55b2fefb835e2ffea7490c8d41aff3988c9c
martinj-jaime <martinjosejaime@gmail.com>
2025-02-13 11:18:42 -0300

    agrego listado de veterinarios

--- pet-clinic-app/petclinitweb/src/main/java/com/example/demo/controllers/VetController.java
+++ pet-clinic-app/petclinitweb/src/main/java/com/example/demo/controllers/VetController.java
@@ -1,15 +1,23 @@
 package com.example.demo.controllers;
 
+import com.example.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
 
     @GetMapping
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 

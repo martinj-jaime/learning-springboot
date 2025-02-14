1f4198f0c323e0ed7004fa8ef8b370f2075d0513
martinj-jaime <martinjosejaime@gmail.com>
2025-02-14 10:59:50 -0300

    agrego visitas y actualizo propietario para relacionarlo con las mascotas

--- pet-clinic-app/petclinicdata/src/main/java/com/example/model/Owner.java
+++ pet-clinic-app/petclinicdata/src/main/java/com/example/model/Owner.java
@@ -1,7 +1,19 @@
 package com.example.model;
+import java.util.Set;
 
 public class Owner extends Person {
+    private Set<Pet> pets;
 
+    public Owner(String firstName, String lastName, Set<Pet> pets) {
+        super(firstName, lastName);
+        this.pets = pets;
+    }
 
+    public Set<Pet> getPets() {
+        return pets;
+    }
 
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }

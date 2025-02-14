c2835bf0efe57056b54f05662bb2c6d596adfbc3
martinj-jaime <martinjosejaime@gmail.com>
2025-02-14 11:04:37 -0300

    agrego especialidad y lo relaciono con los veterinarios

---
+++ pet-clinic-app/petclinicdata/src/main/java/com/example/model/Speciality.java
@@ -0,0 +1,17 @@
+package com.example.model;
+
+public class Speciality extends BaseEntity {
+    private String description;
+
+    public Speciality(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-app/petclinicdata/src/main/java/com/example/model/Vet.java
+++ pet-clinic-app/petclinicdata/src/main/java/com/example/model/Vet.java
@@ -1,11 +1,23 @@
 package com.example.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+    private Set<Speciality> specialities;
+
     public Vet() {
 
     }
 
     public Vet(String firstName, String lastName) {
         super(firstName, lastName);
+    }
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
     }
 }

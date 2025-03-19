c2cd63d82a3d6c3a6bf84e8c9678902758037e0f
martinj-jaime <martinjosejaime@gmail.com>
2025-03-19 11:59:09 -0300

    agrego json endpoint para los veterinarios

--- pet-clinic-app/petclinitweb/src/main/java/com/example/demo/controllers/VetController.java
+++ pet-clinic-app/petclinitweb/src/main/java/com/example/demo/controllers/VetController.java
@@ -1,13 +1,19 @@
 package com.example.demo.controllers;
 
+import com.example.model.Vet;
 import com.example.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 @Controller
-@RequestMapping({ "/vets", "/vets.html" })
+// @RequestMapping({ "/vets", "/vets.html" })
 public class VetController {
     private final VetService vetService;
 
@@ -15,10 +21,17 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @GetMapping
+    @GetMapping({ "/vets", "/vets.html" })
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> listVetsJson() {
+        Set<Vet> vets = vetService.findAll();
+       // vets.add(new Vet(1L, "Vet name", "Vet surname"));
+        return vets;
     }
 
 }

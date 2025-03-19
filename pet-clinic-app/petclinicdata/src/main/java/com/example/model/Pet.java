ee30bab3445e871c9b1038b9b0ae0ed98621a679
martinj-jaime <martinjosejaime@gmail.com>
2025-03-19 10:59:31 -0300

    agrego controlador de visitas

--- pet-clinic-app/petclinicdata/src/main/java/com/example/model/Pet.java
+++ pet-clinic-app/petclinicdata/src/main/java/com/example/model/Pet.java
@@ -33,10 +33,16 @@ public class Pet extends BaseEntity {
 
     public Pet() {}
 
-    public Pet(LocalDate birthDate, PetType petType, Owner owner) {
+    public Pet(Long id, String name, LocalDate birthDate, PetType petType, Owner owner, Set<Visit> visits) {
+        super(id);
+        this.name = name;
         this.birthDate = birthDate;
         this.petType = petType;
         this.owner = owner;
+
+        if(visits == null || visits.size() > 0) {
+            this.visits = visits;
+        }
     }
 
 }
---
+++ pet-clinic-app/petclinitweb/src/main/java/com/example/demo/controllers/VisitController.java
@@ -0,0 +1,74 @@
+package com.example.demo.controllers;
+
+import com.example.model.Pet;
+import com.example.model.Visit;
+import com.example.services.PetService;
+import com.example.services.VisitService;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.beans.PropertyEditorSupport;
+import java.time.LocalDate;
+import java.util.Map;
+
+@Controller
+@RequestMapping({
+        "/owners/{ownerId}/pets/{petId}/visits",
+        "/owners/*/pets/{petId}/visits"
+})
+public class VisitController {
+
+    private final VisitService visitService;
+    private final PetService petService;
+
+    public VisitController(VisitService visitService, PetService petService) {
+        this.visitService = visitService;
+        this.petService = petService;
+    }
+
+    @InitBinder
+    public void dataBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+
+        dataBinder.registerCustomEditor(LocalDate.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException{
+                setValue(LocalDate.parse(text));
+            }
+        });
+    }
+
+
+    @ModelAttribute("visit")
+    public Visit loadPetWithVisit(@PathVariable("petId") Long petId, Map<String, Object> model) {
+        Pet pet = petService.findById(petId);
+        model.put("pet", pet);
+        Visit visit = new Visit();
+        pet.getVisits().add(visit);
+        visit.setPet(pet);
+        return visit;
+    }
+
+    // Spring MVC calls method loadPetWithVisit(...) before initNewVisitForm is called
+    // @GetMapping("/owners/*/pets/{petId}/visits/new")
+    @GetMapping("/new")
+    public String initNewVisitForm(@PathVariable("petId") Long petId, Map<String, Object> model) {
+        return "pets/createOrUpdateVisitForm";
+    }
+
+    // Spring MVC calls method loadPetWithVisit(...) before processNewVisitForm is called
+    // @PostMapping("/owners/{ownerId}/pets/{petId}/visits/new")
+    @PostMapping("/new")
+    public String processNewVisitForm(@Valid Visit visit, BindingResult result) {
+        if (result.hasErrors()) {
+            return "pets/createOrUpdateVisitForm";
+        } else {
+            visitService.save(visit);
+
+            return "redirect:/owners/{ownerId}";
+        }
+    }
+}
